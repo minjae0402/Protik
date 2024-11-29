7785e63cedf619bfd02865b7c56d77174b6accf6
minjae0402 <kangjack0402@gmail.com>
2024-11-30 00:10:47 +0900

    Add : MenuPage

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../styles/MenuItem.css';
+
+interface MenuItemProps {
+    imageSrc: string;
+    title: string;
+    description: string;
+    date: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ imageSrc, title, description, date }) => {
+    return (
+        <button className="menu-item">
+            <img src={imageSrc} alt={title} className="menu-item-image" />
+            <div className="menu-item-text">
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </div>
+            <div className="menu-item-date">
+                {date}
+            </div>
+        </button>
+    );
+};
+
+export default MenuItem;

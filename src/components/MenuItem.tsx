24c957956ee1d801fdb42c717ea367bfbb0fb4cd
minjae0402 <kangjack0402@gmail.com>
2024-11-30 17:40:05 +0900

    Fix : MenuItem Design

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import '../styles/MenuItem.css';
+import { FaLink } from 'react-icons/fa';
 
 interface MenuItemProps {
     imageSrc: string;
     title: string;
     description: string;
     date: string;
+    link: string;
+    daysLeft: string;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ imageSrc, title, description, date }) => {
+const MenuItem: React.FC<MenuItemProps> = ({ imageSrc, title, description, date, link, daysLeft }) => {
     return (
         <>
-            <button className="menu-item">
-                <img src={imageSrc} alt={title} className="menu-item-image" />
+            <div className="menu-item">
+                <div className="menu-item-left">
+                    <img src={imageSrc} alt={title} className="menu-item-image" />
+                    <div className='menu-item-link-line'>
+                        <FaLink className='menu-item-image-Icon' />
+                        <a href={link} target="_blank" className="menu-item-link">
+                            공식 홈페이지
+                        </a>
+                    </div>
+                </div>
                 <div className="menu-item-text">
                     <h2>{title}</h2>
                     <p>{description}</p>
                 </div>
-                <div className="menu-item-date">
-                    {date}
+                <div className="menu-item-right">
+                    <div className="menu-item-date">
+                        {date}
+                    </div>
+                    <div className="menu-item-days-left">
+                        {daysLeft}
+                    </div>
+                    <button className='menu-item-go-practice-btn'>연습하러가기 &gt;</button>
                 </div>
-            </button>
+            </div>
             <hr />
         </>
     );

d1ac09acc6a2c33e7a49c2b32095def5c0ea5a9f
minjae0402 <kangjack0402@gmail.com>
2024-12-05 09:45:19 +0900

    Pull point

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -7,6 +7,8 @@ const Practice: React.FC = () => {
             <h2>튜토리얼 페이지</h2>
             <p>연습하기전 설명서 페이지 입니다! 한번 숙지하고 시작하세요!</p>
             <img src="/Tutorials_001.png" />
+            <h4>1/12</h4>
+            <button>다음</button>
         </div>
     );
 }

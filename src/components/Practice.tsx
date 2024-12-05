603f726b558dafe5d81c1ed683fce7ad5ecf96ed
minjae0402 <kangjack0402@gmail.com>
2024-12-05 09:38:22 +0900

    Add : Practice Page

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -3,11 +3,12 @@ import '../styles/Practice.css';
 
 const Practice: React.FC = () => {
     return (
-        <div className="practice-container">
-            <h1>Practice Page</h1>
-            <p>여기에서 연습을 시작하세요!</p>
+        <div className='practice-container'>
+            <h2>튜토리얼 페이지</h2>
+            <p>연습하기전 설명서 페이지 입니다! 한번 숙지하고 시작하세요!</p>
+            <img src="/Tutorials_001.png" />
         </div>
     );
-};
+}
 
 export default Practice;

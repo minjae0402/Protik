b340fe330e5fd969d1785359dc598e04dbf9e9e8
minjae0402 <kangjack0402@gmail.com>
2024-12-09 10:22:54 +0900

    Add : Timer

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -13,19 +13,16 @@ const Practice: React.FC = () => {
         navigate('/')
     }
     const [page, setPage] = useState(1);
-    const totalPages = 9;
+    const totalPages = 6;
 
     const pageDescriptions = [
         "",
         "",
-        "내용2",
-        "내용3",
-        "내용4",
-        "내용5",
-        "내용6",
-        "내용7",
-        "내용8",
-        "내용9"
+        "원활한 티켓팅을 위해 접속은 크롬으로!",
+        "예매창이 안 뜰 수 있기에 설정에서 팝업 해체하기!",
+        "로그인창이 뜨지 않도록 미리 로그인 확인하기!",
+        "참사를 막기 위해 키보드 한/영 확인!!",
+        "오류를 배제하기 위해 결제는 꼭꼭 무통장으로!",
     ];
 
     const handleNextPage = () => {
@@ -45,13 +42,13 @@ const Practice: React.FC = () => {
             <h2>튜토리얼</h2>
             {page === 1 ? (
                 <div className='intro-content'>
-                    <h3>시작하기 전에</h3>
-                    <p>이 튜토리얼을 통해 티켓팅에서의 주의사항과 꿀팁들을 공부하고 연습해보세요!</p>
+                    <h3>잠깐!! 시작하기 전에</h3>
+                    <p>꼭 알아두셔야 할 팁들을 알려줄게요!</p>
                 </div>
             ) : (
                 <img src={`/Tutorials/Tutorials_00${page.toString().padStart(0, '0')}.png`} alt={`Tutorial ${page}`} />
             )}
-            {page !== 1 && <p>{pageDescriptions[page]}</p>}
+            {page !== 1 && <h4>{pageDescriptions[page]}</h4>}
             <div className='Bottom-container'>
                 <button onClick={handleExitClick}>나가기</button>
                 <div className='Bottom-page-container'>
